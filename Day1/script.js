8e78b8fded7e6bb47fef6839a373faa12a388367
chellis111 <abajoadesoye2@gmail.com>
2024-08-29 16:42:46 +0100

    Feat: Day 1 of learning javascript

---
+++ Day1/script.js
@@ -0,0 +1,33 @@
+console.log("day 1 of javascript")
+console.log("hei dfjvjfd7s78*&***dsnjnjd")
+console.log('fjfjfjf')
+console.log(`ggjjgj`)
+console.log(474783)
+console.log(true)
+console.log(false)
+
+// let firstName
+// console.log(firstName)
+// two-words
+// two_words
+// twoWords
+// twowords
+
+
+//Type of datas
+console.log(typeof 'Asabeneh') // string
+console.log(typeof 5) // number
+console.log(typeof true) // boolean
+console.log(typeof null) // object type
+console.log(typeof undefined) // undefined
+
+
+// Declaring different variables of different data types
+let firstName = 'Asabeneh' // first name of a person
+let lastName = 'Yetayeh' // last name of a person
+let country = 'Finland' // country
+let city = 'Helsinki' // capital city
+let age = 100 // age in years
+let isMarried = true
+
+console.log(firstName, lastName, country, city, age, isMarried)
